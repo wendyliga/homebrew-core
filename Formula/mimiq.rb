abda4d3d13e26477d78fb7628100257a133900a8
Wendy Liga <agilydnew@gmail.com>
2020-03-01 19:08:19 +0700

    update sha256

--- Formula/mimiq.rb
+++ Formula/mimiq.rb
@@ -2,7 +2,7 @@ class Mimiq < Formula
     desc "mimiq is simple executable to record your Xcode simulator and convert it to GIF"
     homepage "https://github.com/wendyliga/mimiq"
     url "https://github.com/wendyliga/mimiq/releases/download/0.0.1/mimiq.tar.gz"
-    sha256 "b80fec03bb31f6d3cec785df03ee355655caae068512faed596abe2cb1fc4f27"
+    sha256 "d09ee0a6411c449543be9a071ebca5330820f2b84c2d3b775d776ba83fdbf035"
   
     bottle :unneeded
 
