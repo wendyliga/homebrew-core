fb0c16aae3e3313e744cdc8ca3f865027b81826f
Wendy Liga <agilydnew@gmail.com>
2020-03-29 20:57:50 +0700

    update formula

--- Formula/mimiq.rb
+++ Formula/mimiq.rb
@@ -2,19 +2,31 @@ class Mimiq < Formula
     desc "A Simple Xcode Simulator GIF Recoder"
     homepage "https://github.com/wendyliga/mimiq"
     url "https://github.com/wendyliga/mimiq.git", 
-        :tag      => "0.2.0"
+        :tag      => "0.2.2",
+        :revision => "c93af7421fe2cbbf5945911e41634256f75849b5"
     head "https://github.com/wendyliga/mimiq.git"
+
+    bottle do
+        cellar :any_skip_relocation
+    end
   
-    depends_on :ffmpeg => :recommended
+    depends_on "ffmpeg" => :recommended
     depends_on :xcode => "11.0"
   
     def install
       system "make", "install", "prefix=#{prefix}"
     end
-  
-    test do
-      # system "#{bin}/mimiq --help"
-      system true
-    end
+
+    def caveats
+        <<~EOS
+          Start record your Simulator by executing:
+            mimiq
+            
+          To see how to use mimiq, you can execute:
+            mimiq --help
+
+          For more detail, please check https://github.com/wendyliga/mimiq
+        EOS
+      end
   end
   
